3ad61f5c1ea7e4278af6daccc9a68d8ff8400fb1
Taofeek <bellotaophyc@outlook.com>
2025-06-21 12:33:19 +0200

    omment out the @service annotation

--- src/main/java/cz/mendelu/service/RoleServiceImpl.java
+++ src/main/java/cz/mendelu/service/RoleServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+//@Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleDao roleDao;
