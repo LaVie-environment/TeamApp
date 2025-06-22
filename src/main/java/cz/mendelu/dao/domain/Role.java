e0080251129023309053b02590c97a70bdb8d63f
Taofeek <bellotaophyc@outlook.com>
2025-06-22 14:58:34 +0200

    Update Role.java to a proper JPA entity

--- src/main/java/cz/mendelu/dao/domain/Role.java
+++ src/main/java/cz/mendelu/dao/domain/Role.java
@@ -1,28 +1,35 @@
 package cz.mendelu.dao.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
 
-    public Role() {
-    }
+    public Role() {}
 
-    public Role(Long id, String name) {
-        this.id = id;
+    public Role(String name) {
         this.name = name;
     }
 
+    // Getters and setters
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
